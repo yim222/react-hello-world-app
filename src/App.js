62f62d60855b61647b4b2eaa9b0745eca61d0b5b
lingar <yimprogramming@gmail.com>
2021-04-27 09:28:40 +0300

    Initial lingar commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import HelloWorldList from './HelloWorldList';
-
+const x ="l";
 const App = () => {
   return (
   <div className="App">
+    <h1>Lingar take it</h1>
   <HelloWorldList />
   </div>
   );
