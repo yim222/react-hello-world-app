19bb0042ec99779465c235f5caaaa5b9b2f26e30
lingar <yimprogramming@gmail.com>
2021-04-27 09:45:01 +0300

    Adding things

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ const x ="l";
 const App = () => {
   return (
   <div className="App">
-    <h1>Lingar take it</h1>
+    <h1>Lingar take it from
+    <a href = "https://github.com/Syknapse/react-hello-world-app">here</a></h1>
   <HelloWorldList />
   </div>
   );
